6e53ae9c980fbf5c6c9918e59dcc4a1ddf1aac08
Hristo Yankov <ico.yankov@gmail.com>
2016-08-01 08:51:53 +0300

    Finish initial category model version

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,10 @@ ActiveRecord::Schema.define(version: 20160717124234) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "parent_category_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["parent_category_id"], name: "index_categories_on_parent_category_id"
   end
 
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,15 +3,19 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   subject { described_class.new }
 
-  it "is valid with valid name" do
-    subject.name = "Lunch"
-    expect(subject).to be_valid
+  describe "Validations" do
+    it "is valid with valid name" do
+      subject.name = "Lunch"
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without name" do
+      expect(subject).to_not be_valid
+    end
   end
 
-  it "is not valid without name" do
-    expect(subject).to_not be_valid
+  describe "Associations" do
+    it { should belong_to(:parent_category) }
+    it { should have_many(:subcategories) }
   end
-
-  it "is valid with valid children categories"
-  it "is valid with parent category"
 end

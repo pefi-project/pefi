f31e14d5fa05fdb8adb3047e10d5623a8dd79403
Hristo Yankov <ico.yankov@gmail.com>
2016-08-08 22:49:39 +0300

    Add initial version of all CRUD methods for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,26 +3,44 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
-  def create
-    @category = Category.new(params.require(:category).permit(:name))
-    @category.save
-    redirect_to @category
-  end
-
   def new
     @category = Category.new
   end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
   def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      redirect_to categories_path
+    else
+      render 'edit'
+    end
   end
 
   def show
     @category = Category.find(params[:id])
   end
 
-  def update
+  def destroy
+    Category.find(params[:id]).destroy
+    redirect_to categories_path
   end
 
-  def destroy
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end

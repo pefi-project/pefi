c32d5bb6764161aae7783b2b2df6493ac1cbc244
Hristo Yankov <ico.yankov@gmail.com>
2016-09-11 20:45:58 +0300

    Add all basic CRUD operations to earnings

--- app/controllers/earnings_controller.rb
+++ app/controllers/earnings_controller.rb
@@ -1,2 +1,48 @@
 class EarningsController < ApplicationController
+
+  def index
+    @earnings = Earning.all
+  end
+
+  def new
+    @earning = Earning.new
+  end
+
+  def create
+    @earning = Earning.new(earning_params)
+    @earning.date = Time.zone.now
+    if @earning.save
+      redirect_to @earning
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @earning = Earning.find(params[:id])
+  end
+
+  def edit
+    @earning = Earning.find(params[:id])
+  end
+
+  def update
+    @earning = Earning.find(params[:id])
+    if @earning.update_attributes(earning_params)
+      redirect_to earnings_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Earning.find(params[:id]).destroy
+    redirect_to earnings_path
+  end
+
+  private
+
+  def earning_params
+    params.require(:earning).permit(:name, :amount_cents)
+  end
 end

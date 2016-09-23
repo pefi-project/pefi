542e3f82ccb6f9e66db0a962a9bb2eed5fc62835
Hristo Yankov <ico.yankov@gmail.com>
2016-09-23 16:44:55 +0300

    Refactor index/show views into partials

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -6,4 +6,8 @@ class Expense < ApplicationRecord
   validates :name, presence: true
   validates :date, presence: true
   validates :price_cents, presence: true, numericality: { greater_than: 0 }
+
+  def description
+    "#{name} (#{price})"
+  end
 end

dad772169331d2bcb183fa71537a345ee5ee25c5
Hristo Yankov <ico.yankov@gmail.com>
2016-10-26 12:22:20 +0300

    Make root route redirection.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-  def hello
-    render html: 'Hello World!'
+  def home
+    redirect_to expenses_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'expenses#index'
+  root 'application#home'
 
   resources :categories
   resources :earnings

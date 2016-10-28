43bab362d6da13f1770387c87a1cf3e5c9a3d80d
Hristo Yankov <ico.yankov@gmail.com>
2016-10-28 21:33:44 +0300

    Add not signed in scenario to categories test

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -1,31 +1,40 @@
 require 'rails_helper'
 
 feature 'Category management', js: true do
-  scenario 'Creating new category' do
-    user = FactoryGirl.create(:user)
-    login_as(user, scope: :user)
-
-    visit new_category_path
-
-    within('#new_category') do
-      fill_in 'category[name]', with: 'Test'
-      click_on 'Create Category'
+  context 'when signed in' do
+    before(:each) do
+      user = FactoryGirl.create(:user)
+      login_as(user, scope: :user)
     end
-    new_category = Category.last
-    expect(new_category).to be
-    expect(new_category.name).to eq('Test')
+
+    scenario 'Creating new category' do
+      visit new_category_path
+
+      within('#new_category') do
+        fill_in 'category[name]', with: 'Test'
+        click_on 'Create Category'
+      end
+      new_category = Category.last
+      expect(new_category).to be
+      expect(new_category.name).to eq('Test')
+    end
+
+    scenario 'List categories' do
+      create :category, name: 'Category 1'
+      create :category, name: 'Category 2'
+
+      visit categories_path
+
+      expect(page).to have_content('Category 1')
+      expect(page).to have_content('Category 2')
+    end
   end
 
-  scenario 'List categories' do
-    user = FactoryGirl.create(:user)
-    login_as(user, scope: :user)
+  context 'when not signed in' do
+    scenario 'redirect you to log in page' do
+      visit categories_path
 
-    create :category, name: 'Category 1'
-    create :category, name: 'Category 2'
-
-    visit categories_path
-
-    expect(page).to have_content('Category 1')
-    expect(page).to have_content('Category 2')
+      expect(page).to have_button('Log in')
+    end
   end
 end
